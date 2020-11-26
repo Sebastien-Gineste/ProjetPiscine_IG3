d04c83404027f367855048495d3fccf1983b6c7c
Sebastien-Gineste <sebastien.gineste@etu.umontpellier.fr>
2020-11-26 21:24:38 +0100

    Frontend début - menu principale + affichage en fonction de login et admin

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,70 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id= "nav">
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand href="#">
+          <b-link class="navbar-brand" to="/">Accueil</b-link>
+        </b-navbar-brand>
+       <!-- <b-navbar-brand href="#"><b-img :src="require('./assets/polytech.png')" fluid alt="Responsive image"></b-img></b-navbar-brand> -->
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <li v-if="isUser()" class="nav-item"><b-link class="nav-link" to="/about">S'inscrire à une soutance</b-link></li>
+            <li v-if="isUser()" class="nav-item"><b-link class="nav-link" to="/Groupe">Gestion du groupe</b-link></li>
+            <li v-if="isAdmin()" class="nav-item"><b-link class="nav-link" to="/Evenement">Gestion des événements</b-link></li>
+            <li v-if="isAdmin()" class="nav-item"><b-link class="nav-link" to="/Planning">Gestion des plannings</b-link></li>
+            <li v-if="isAdmin()" class="nav-item"><b-link class="nav-link" to="/Tuteur">Gesion des tuteus</b-link></li>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item-dropdown right>
+              <!-- Using 'button-content' slot -->
+              <template #button-content>
+                <em> <b-icon icon="person-fill"></b-icon></em>
+              </template>
+              <li v-if="!isUser()" role="presentation"><b-link role="menuitem" class="dropdown-item" to="/Connexion">Connexion</b-link></li> <!-- Lien vers une page sous l'icône du profile -->
+              <li v-if="!isUser() || isAdmin()" role="presentation"><b-link role="menuitem" class="dropdown-item" to="/Etudiant/Création_compte">Créer un compte</b-link></li> <!-- Lien vers une page sous l'icône du profile -->
+              <li v-if="isUser()" role="presentation"><b-link role="menuitem" class="dropdown-item" to="/Etudiant/Compte">Mon compte</b-link></li> <!-- Lien vers une page sous l'icône du profile -->
+              <li v-if="isUser()" role="presentation"><b-link role="menuitem" class="dropdown-item" to="/Deconnexion">Déconnexion</b-link></li> <!-- Lien vers une page sous l'icône du profile -->
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
     </div>
-    <router-view/>
+
+    
+
+    <router-view/> <!-- Affiche la page du router sélectionner -->
+
+
+
   </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+    computed: {
+        ...mapState({
+            user: 'user',
+            admin: 'admin',
+            token: 'token'
+        })
+    },
+    methods: {
+     isAdmin(){
+       return this.admin; 
+     },
+     isUser(){
+       return this.user != null;
+     }
+  }
+}
+
+</script>
 
 <style lang="scss">
 #app {
@@ -17,15 +75,20 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+.dropdown-menu{
+  background-color: #17a2b8 !important;
+}
 
+.dropdown-item:hover, .dropdown-item:focus {
+  background-color: transparent !important;
+}
+#nav {
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: white;
 
-    &.router-link-exact-active {
-      color: #42b983;
+    &.router-link-exact-active {   // couleur du lien activé (qui pointe sur la page actuel)
+      color:black;
     }
   }
 }
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+// on va mettre ici les infos de la personne qui est connecté
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user : "jean",
+    admin : false,
+    token : "token"
   },
   mutations: {
   },
