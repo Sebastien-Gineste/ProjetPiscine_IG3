9033cadecee515fb6c8ad8151204ccbd0bb68a60
Sebastien-Gineste <sebastien.gineste@etu.umontpellier.fr>
2020-12-01 12:40:40 +0100

    Back : Ajout des routes pour le groupe_projet ainsi que ses fonctions à développer

---
+++ Backend/controllers/groupe_projet.js
@@ -0,0 +1,52 @@
+const Groupe = require('../models/groupe_jprojet');
+const Composer = require('../models/composerGroupe_Etudiant');
+const Etudiant = require('../models/etudiant');
+const errorModel = require("../models/model");
+
+//A faire
+exports.selectAll = (req, res, next) => {
+  res.status(500).send('Pas encore fait')
+};
+
+//A faire
+exports.save = (req, res, next) => {
+  res.status(500).send('Pas encore fait')
+};
+
+//A faire
+exports.select = (req, res, next) => {
+  console.log(req.params.id)
+  res.status(500).send('Pas encore fait')
+};
+
+//A faire
+exports.update = (req, res, next) => {
+  console.log(req.params.id)
+  res.status(500).send('Pas encore fait')
+};
+
+//A faire
+exports.delete = (req, res, next) => {
+  console.log(req.params.id)
+  res.status(500).send('Pas encore fait')
+};
+
+//A faire
+exports.selectAllEtudiant = (req, res, next) => {
+  console.log(req.params.id)
+  res.status(500).send('Pas encore fait')
+};
+
+//A faire
+exports.ajoutEtudiant = (req, res, next) => {
+  console.log(req.params.id)
+  res.status(500).send('Pas encore fait')
+};
+
+
+//A faire
+exports.removeEtudiant = (req, res, next) => {
+  console.log(req.params.id)
+  res.status(500).send('Pas encore fait')
+};
+
--- Backend/routes/groupe_projet.js
+++ Backend/routes/groupe_projet.js
@@ -4,15 +4,16 @@ const router = express.Router();
 
 const auth = require('../middleware/auth');
 
-const multer = require('../middleware/multer-config');
+const groupeCtrl = require('../controllers/groupe_projet');
 
-const stuffCtrl = require('../controllers/stuff');
-
-router.get('/', auth, stuffCtrl.getAllStuff);
-router.post('/', auth, multer,  stuffCtrl.createThing);
-router.get('/:id', auth, stuffCtrl.getOneThing);
-router.put('/:id', auth, multer, stuffCtrl.modifyThing);
-router.delete('/:id', auth, stuffCtrl.deleteThing);
+router.get('/', auth.verifyToken, groupeCtrl.selectAll);                                     //récupère tous les groupes (admin?)
+router.get('/etudiants',auth.verifyToken, groupeCtrl.selectAllEtudiant)                      //récupère tous les étudians (id, nom, premnom) n'ayant pas encore de groupe pour la promo (Autocompletion quand un étudiant ajoutera d'autre étudiant à son groupe)
+router.get('/:id', auth.verifyToken, groupeCtrl.select);                                     //Renvoie les information d'un seul groupe identifier par son "id"
+router.post('/', auth.verifyToken, multer,  groupeCtrl.save);                                //Créer un groupe dans la BD
+router.post('/:id/addEtudiant',auth.verifyToken, multer,  groupeCtrl.ajoutEtudiant)          //Ajout un étudiant dans le groupe identifier avec son "id" (ajout une ligne dans la table composer)
+router.put('/:id', auth.verifyToken, multer, groupeCtrl.update);                             //Modifier un groupe représenter par son id
+router.delete('/:id', auth.verifyToken, groupeCtrl.delete);                                  //Supprimer un groupe représenter par son id
+router.delete('/:id/removeEtudiant',auth.verifyToken, multer,  groupeCtrl.removeEtudiant)    //Supprimer un étudiant dans le groupe identifier avec son "id" (supprime une ligne dans la table composer)
 
 
 module.exports = router
