ba5c0bf382f4264a5209d58dc485ec47ccf97db9
Sebastien-Gineste <47150778+Sebastien-Gineste@users.noreply.github.com>
2021-01-09 11:13:23 +0100

    Rewrite localhost for merge

--- Backend/app.js
+++ Backend/app.js
@@ -16,7 +16,7 @@ const promoRoutes = require('./routes/promo');
 const app = express();
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8081'); // *
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080'); // *
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     res.setHeader("Access-Control-Allow-Credentials", true);
