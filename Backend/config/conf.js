46f5e93a1a37e6a774cd829dcf7d5867512aa329
Sebastien-Gineste <sebastien.gineste@etu.umontpellier.fr>
2021-01-16 11:28:32 +0100

    config db

--- Backend/config/conf.js
+++ Backend/config/conf.js
@@ -1,11 +1,17 @@
 const Pool = require('pg').Pool
-
+/*
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASS,
   port: 5432,
+})
+*/
+
+const connectionString = process.env.DATABASE_URL
+const pool = new Pool({
+  connectionString
 })
 
 pool.connect()
